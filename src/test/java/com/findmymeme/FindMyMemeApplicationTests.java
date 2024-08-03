f87ec1cc11bf9997e010c892c872ff93c12e6b43
acycoco <pink10257@naver.com>
2024-08-03 20:47:00 +0900

    chore: 초기 프로젝트 설정

---
+++ src/test/java/com/findmymeme/FindMyMemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmymeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyMemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
