4d86cc7a9b4f8227658c048c07f90ac096dc01fe
acycoco <pink10257@naver.com>
2024-08-09 03:22:50 +0900

    feat: 에러 핸들러 응답을 ApiResponse를 이용하도록 변경
    related-to: #17

--- src/main/java/com/findmymeme/exception/FindMyMemeExceptionHandler.java
+++ src/main/java/com/findmymeme/exception/FindMyMemeExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.findmymeme.exception;
 
+import com.findmymeme.response.ApiResponse;
+import com.findmymeme.response.ResponseUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,16 +10,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class FindMyMemeExceptionHandler {
 
+    private final MessageSource messageSource;
+
     @ExceptionHandler(FindMyMemeException.class)
-    public ResponseEntity<ErrorResponse> handleFindMyMemeException(final FindMyMemeException e) {
+    public ResponseEntity<ApiResponse<Void>> handleFindMyMemeException(final FindMyMemeException e) {
         final ErrorCode errorCode = e.getErrorCode();
-        final ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(errorCode.getHttpStatus())
-                .message(errorCode.getMessage())
-                .build();
-        log.error("Error: code={}, message={}", errorCode, errorResponse.getMessage(), e);
-        return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
+        log.error("Error: code={}, message={}", errorCode, errorCode.getMessage(), e);
+        return ResponseUtil.error(null, errorCode); //TODO null처리 변경
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleGeneralException(final Exception e) {
+        log.error("Unexpected error occurred: {}", e.getMessage(), e);
+        return ResponseUtil.error(null, ErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
