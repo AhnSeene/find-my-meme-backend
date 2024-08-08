7021626d5a8f17a8735410d9d5e3ea1dcac02d58
acycoco <pink10257@naver.com>
2024-08-09 03:38:42 +0900

    test: UserController 회원가입 테스트 추가
    - 유효성검증 성공한 경우 테스트
    - 실패한 경우 테스트
    
    related-to: #17, #8

--- src/main/java/com/findmymeme/user/dto/UserResponse.java
+++ src/main/java/com/findmymeme/user/dto/UserResponse.java
@@ -1,13 +1,16 @@
 package com.findmymeme.user.dto;
 
 import com.findmymeme.user.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserResponse {
 
     private final String username;
     private final String email;
+
 
     public UserResponse(final User user) {
         this.username = user.getUsername();
