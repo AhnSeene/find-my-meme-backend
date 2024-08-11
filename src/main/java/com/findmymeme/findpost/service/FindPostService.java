2803b753760469b847cf1b363d487579db215fe8
acycoco <pink10257@naver.com>
2024-08-11 18:55:27 +0900

    feat: content의 img태그 변경, 영구로 파일이동, FindPostImage생성
    - html을 파싱해서 img태그의 임시 url추출
    - 임시 디렉토리의 파일을 영구로 이동
    - img태그를 영구 url로 변경
    - FileMeta를 사용해서 FindPostImages저장
    
    related-to: #31

--- src/main/java/com/findmymeme/findpost/service/FindPostService.java
+++ src/main/java/com/findmymeme/findpost/service/FindPostService.java
@@ -43,8 +43,45 @@ public class FindPostService {
 
         FindPost findPost = createFindPost(request, user);
 
+        Document doc = Jsoup.parse(request.getContent());
+        List<FindPostImage> findPostImages = processImages(doc, findPost);
+
+        findPost.changeContent(doc.body().html());
+        FindPost savedFindPost = findPostRepository.save(findPost);
+        findPostImageRepository.saveAll(findPostImages);
+
         return new FindPostUploadResponse(savedFindPost);
     }
+
+    /**
+     * 주어진 문서에서 이미지를 처리합니다:
+     * <ul>
+     *     <li>임시 이미지 URL을 영구 이미지 URL로 변경합니다.</li>
+     *     <li>파일을 임시 저장소에서 영구 저장소로 이동합니다.</li>
+     *     <li>처리된 이미지에 대한 FindPostImage 엔티티를 생성합니다.</li>
+     * </ul>
+     *
+     * @param doc      이미지 태그가 포함된 콘텐츠를 담고 있는 Document 객체
+     * @param findPost 이미지와 연관될 FindPost 엔티티
+     * @return 처리된 이미지를 위한 FindPostImage 엔티티의 리스트
+     */
+    private List<FindPostImage> processImages(Document doc, FindPost findPost) {
+        List<FindPostImage> findPostImages = new ArrayList<>();
+        doc.select(IMG_TAG).forEach(img -> {
+            String tempUrl = img.attr(IMG_SRC);
+            String storedFilename = extractFilenameFromUrl(tempUrl);
+            String permanentFilename = fileStorageService.moveFileToPermanent(storedFilename);
+            String permanentUrl = fileStorageService.getPermanentFileUrl(permanentFilename);
+
+            img.attr(IMG_SRC, permanentUrl);
+
+            FileMeta fileMeta = findFileMetaByFileUrl(tempUrl);
+            FindPostImage findPostImage = createFindPostImage(findPost, permanentUrl, fileMeta);
+            findPostImages.add(findPostImage);
+        });
+        return findPostImages;
+    }
+
     private FindPost createFindPost(FindPostUploadRequest request, User user) {
         return FindPost.builder()
                 .title(request.getTitle())
@@ -52,8 +89,27 @@ public class FindPostService {
                 .user(user)
                 .build();
     }
+
+    private FindPostImage createFindPostImage(FindPost findPost, String permanentUrl, FileMeta fileMeta) {
+        return FindPostImage.builder()
+                .imageUrl(permanentUrl)
+                .originalFilename(fileMeta.getOriginalFilename())
+                .findPost(findPost)
+                .build();
+    }
+
+    private FileMeta findFileMetaByFileUrl(String tempUrl) {
+        return fileMetaRepository.findByFileUrl(tempUrl)
+                .orElseThrow(() -> new FindMyMemeException(ErrorCode.NOT_FOUND_FILE_META));
+    }
+
     private User findUserById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new FindMyMemeException(ErrorCode.NOT_FOUND_USER));
     }
 
+
+    private String extractFilenameFromUrl(String url) {
+        return url.substring(url.lastIndexOf(URL_SLASH) + 1);
+    }
+}
