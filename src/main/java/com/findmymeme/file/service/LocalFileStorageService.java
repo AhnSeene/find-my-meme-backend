66ebf5d5996d0fdb5828cb31659e785687be133d
acycoco <pink10257@naver.com>
2024-08-11 17:34:35 +0900

    feat: 임시디렉토리에 파일 저장, 삭제 기능
    related-to: #27

---
+++ src/main/java/com/findmymeme/file/service/LocalFileStorageService.java
@@ -0,0 +1,60 @@
+package com.findmymeme.file.service;
+
+import com.findmymeme.exception.ErrorCode;
+import com.findmymeme.exception.FindMyMemeException;
+import com.findmymeme.file.exception.FileStorageException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+@Slf4j
+@Service
+@Transactional
+public class LocalFileStorageService implements FileStorageService {
+
+    private static final String URL_FORMAT = "%s/%s";
+    private final String tempDir;
+    private final String baseDir;
+    private final String permanentDir;
+
+    public LocalFileStorageService(
+            @Value("${file.upload.temp-dir}") String tempDir,
+            @Value("${file.upload.image-dir}") String permanentDir,
+            @Value("${file.base-dir}") String baseDir
+    ) {
+        this.tempDir = tempDir;
+        this.permanentDir = permanentDir;
+        this.baseDir = baseDir;
+    }
+
+    @Override
+    public String storeTempFile(MultipartFile file) {
+        String storedFilename = generateStoredFilename(file.getOriginalFilename());
+        Path filePath = Paths.get(baseDir, tempDir, storedFilename);
+        try {
+            Files.copy(file.getInputStream(), filePath);
+        } catch (FileAlreadyExistsException e) {
+            throw new FileStorageException(ErrorCode.ALREADY_EXIST_FILE);
+        } catch (IOException e) {
+            throw new FileStorageException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+        return storedFilename;
+    }
+
+    @Override
+    public void deleteTempFile(String storedFilename) {
+        Path filePath = Paths.get(baseDir, tempDir, storedFilename);
+        try {
+            Files.deleteIfExists(filePath);
+        } catch (NoSuchFileException e) {
+            throw new FileStorageException(ErrorCode.NOT_FOUND_FILE);
+        } catch (IOException e) {
+            throw new FileStorageException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
