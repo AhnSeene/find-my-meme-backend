e7395ad2c0595d1050e1dc8411c816596a8098b6
acycoco <pink10257@naver.com>
2024-08-14 02:53:53 +0900

    test: FindPostManageService 단건 조회 테스트
    - 성공한 경우
    - 글의 주인이 아닌 경우
    - 존재하지 않는 글을 조회 시
    
    related-to: #35

--- src/test/java/com/findmymeme/findpost/service/FindPostManageServiceTest.java
+++ src/test/java/com/findmymeme/findpost/service/FindPostManageServiceTest.java
@@ -1,17 +1,22 @@
 package com.findmymeme.findpost.service;
 
+import com.findmymeme.exception.ErrorCode;
+import com.findmymeme.exception.FindMyMemeException;
 import com.findmymeme.findpost.domain.FindPost;
 import com.findmymeme.findpost.domain.FindStatus;
 import com.findmymeme.findpost.dto.FindPostGetResponse;
+import com.findmymeme.findpost.dto.FindPostSummaryResponse;
 import com.findmymeme.findpost.repository.FindPostRepository;
 import com.findmymeme.user.User;
 import com.findmymeme.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -44,10 +49,12 @@ class FindPostManageServiceTest {
 
         String title = "test title";
         String content = "test content";
+        String htmlContent = "<p>test content</p>";
         FindPost savedFindPost = findPostRepository.save(
                 FindPost.builder()
                         .title(title)
                         .content(content)
+                        .htmlContent(htmlContent)
                         .user(savedUser)
                         .build()
         );
@@ -55,11 +62,90 @@ class FindPostManageServiceTest {
         // when
         FindPostGetResponse response = findPostManageService.getFindPost(savedFindPost.getId(), savedUser.getId());
 
+        //then
         assertThat(response).isNotNull();
         assertThat(response.getTitle()).isEqualTo(title);
-        assertThat(response.getContent()).isEqualTo(content);
+        assertThat(response.getHtmlContent()).isEqualTo(htmlContent);
+        assertThat(response.getHtmlContent()).isEqualTo(htmlContent);
         assertThat(response.getStatus()).isEqualTo(FindStatus.FIND);
         assertThat(response.isOwner()).isTrue();
+        assertThat(response.getCreatedAt()).isNotNull();
+        assertThat(response.getUpdatedAt()).isNotNull();
+    }
+
+    @Test
+    void FindPost_글의_주인이_아닌_경우_조회_성공() {
+        // given
+        String ownerUsername = "ownerUser";
+        String ownerEmail = "ownerUser@example.com";
+        String ownerPassword = "encodedPassword";
+
+        User ownerUser = userRepository.save(
+                User.builder()
+                        .username(ownerUsername)
+                        .password(ownerPassword)
+                        .email(ownerEmail)
+                        .build()
+        );
+
+        String otherUsername = "otherUser";
+        String otherEmail = "otherUser@example.com";
+        String otherPassword = "encodedPassword";
+
+        User otherUser = userRepository.save(
+                User.builder()
+                        .username(otherUsername)
+                        .password(otherPassword)
+                        .email(otherEmail)
+                        .build()
+        );
+
+        String title = "test title";
+        String content = "test content";
+        String htmlContent = "<p>test content</p>";
+
+        FindPost savedFindPost = findPostRepository.save(
+                FindPost.builder()
+                        .title(title)
+                        .content(content)
+                        .htmlContent(htmlContent)
+                        .user(ownerUser)
+                        .build()
+        );
+
+        // when
+        FindPostGetResponse response = findPostManageService.getFindPost(savedFindPost.getId(), otherUser.getId());
+
+        // then
+        assertThat(response).isNotNull();
+        assertThat(response.getTitle()).isEqualTo(title);
+        assertThat(response.getHtmlContent()).isEqualTo(htmlContent);
+        assertThat(response.getStatus()).isEqualTo(FindStatus.FIND);
+        assertThat(response.isOwner()).isFalse();
+    }
+
+    @Test
+    void FindPost_존재하지_않는_ID_조회_실패() {
+        // given
+        String username = "newUser";
+        String email = "newUser@example.com";
+        String encodedPassword = "encodedPassword";
+
+        User savedUser = userRepository.save(
+                User.builder()
+                        .username(username)
+                        .password(encodedPassword)
+                        .email(email)
+                        .build()
+        );
+
+        // when & then
+        Long invalidFindPostId = 1L;
+        assertThatThrownBy(() -> findPostManageService.getFindPost(invalidFindPostId, savedUser.getId()))
+                .isInstanceOf(FindMyMemeException.class)
+                .hasMessageContaining(ErrorCode.NOT_FOUND_FIND_POST.getMessage());
+    }
+
 
     }
 
