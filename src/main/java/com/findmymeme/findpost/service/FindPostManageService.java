d153077e1114a2ef9dd859c927157270ced2b0a3
acycoco <pink10257@naver.com>
2024-08-14 03:02:46 +0900

    feat: FindPostManageService 페이지 조회
    - 페이지 조회 생성일자 기준 정렬
    - 단건 조회도 변경된 findWithUserById로 변경

--- src/main/java/com/findmymeme/findpost/service/FindPostManageService.java
+++ src/main/java/com/findmymeme/findpost/service/FindPostManageService.java
@@ -4,10 +4,15 @@ import com.findmymeme.exception.ErrorCode;
 import com.findmymeme.exception.FindMyMemeException;
 import com.findmymeme.findpost.domain.FindPost;
 import com.findmymeme.findpost.dto.FindPostGetResponse;
+import com.findmymeme.findpost.dto.FindPostSummaryResponse;
 import com.findmymeme.findpost.repository.FindPostRepository;
 import com.findmymeme.user.User;
 import com.findmymeme.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,10 +28,16 @@ public class FindPostManageService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new FindMyMemeException(ErrorCode.NOT_FOUND_USER));
 
-        FindPost findPost = findPostRepository.findById(findPostId)
+        FindPost findPost = findPostRepository.findWithUserById(findPostId)
                 .orElseThrow(() -> new FindMyMemeException(ErrorCode.NOT_FOUND_FIND_POST));
 
         return new FindPostGetResponse(findPost, findPost.isOwner(user));
+    }
+
+    public Page<FindPostSummaryResponse> getFindPosts(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        return findPostRepository.findAll(pageable)
+                .map(FindPostSummaryResponse::new);
     }
 
 }
