ce67112f6a3daa7c7001e15692bedc9dc3331a8c
acycoco <pink10257@naver.com>
2024-08-14 02:47:45 +0900

    feat: htmlContent 필드 생성, dto 필드 변경

--- src/main/java/com/findmymeme/findpost/dto/FindPostGetResponse.java
+++ src/main/java/com/findmymeme/findpost/dto/FindPostGetResponse.java
@@ -3,28 +3,41 @@ package com.findmymeme.findpost.dto;
 import com.findmymeme.findpost.domain.FindPost;
 import com.findmymeme.findpost.domain.FindStatus;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FindPostGetResponse {
 
     private String title;
     private String htmlContent;
     private FindStatus status;
+    private String username;
     private boolean owner;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public FindPostGetResponse(FindPost findPost, boolean isOwner) {
         this.title = findPost.getTitle();
-        this.htmlContent = findPost.getContent();
+        this.htmlContent = findPost.getHtmlContent();
         this.status = findPost.getFindStatus();
+        this.username = findPost.getUser().getUsername();
         this.owner = isOwner;
         this.createdAt = findPost.getCreatedAt();
         this.updatedAt = findPost.getUpdatedAt();
+    }
+
+    @Builder
+    public FindPostGetResponse(String title, String htmlContent, FindStatus status, String username, boolean owner, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.title = title;
+        this.htmlContent = htmlContent;
+        this.status = status;
+        this.username = username;
+        this.owner = owner;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 }
