520541d525a6de3ce2c3788bf435adecfd2987d0
acycoco <pink10257@naver.com>
2024-08-14 21:12:12 +0900

    feat: FindPostUpdateRequest, Response dto 생성
    - Request dto에 유효성 검증 추가
    
    related-to: #35

--- src/main/java/com/findmymeme/findpost/dto/FindPostUploadRequest.java
+++ src/main/java/com/findmymeme/findpost/dto/FindPostUploadRequest.java
@@ -1,14 +1,18 @@
 package com.findmymeme.findpost.dto;
 
 import com.findmymeme.findpost.domain.FindStatus;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class FindPostUploadRequest {
 
+    @NotBlank(message = "{title.notblank}")
     private String title;
+    @NotBlank(message = "{htmlContent.notblank}")
     private String htmlContent;
+    @NotBlank(message = "{content.notblank}")
     private String content;
 
     @Builder
