6bcd1cb5a630b0e84abdc5ff73b948b08677515a
acycoco <pink10257@naver.com>
2024-08-16 01:25:48 +0900

    feat: FindPostComment관련 SuccessCode, ErrorCode 생성
    related-to: #44

--- src/main/java/com/findmymeme/response/SuccessCode.java
+++ src/main/java/com/findmymeme/response/SuccessCode.java
@@ -10,7 +10,11 @@ public enum SuccessCode {
     FIND_POST_UPLOAD(HttpStatus.CREATED),
     FIND_POST_GET(HttpStatus.OK),
     FIND_POST_LIST(HttpStatus.OK),
-    FIND_POST_UPDATE(HttpStatus.OK);
+    FIND_POST_UPDATE(HttpStatus.OK),
+    FIND_POST_COMMENT_UPLOAD(HttpStatus.CREATED),
+    FIND_POST_COMMENT_GET(HttpStatus.OK),
+    FIND_POST_COMMENT_LIST(HttpStatus.OK),
+    FIND_POST_COMMENT_UPDATE(HttpStatus.OK);
 
     private final HttpStatus status;
     private static final ResourceBundle messages = ResourceBundle.getBundle("messages");
