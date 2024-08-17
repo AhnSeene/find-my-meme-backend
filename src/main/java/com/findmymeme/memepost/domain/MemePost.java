7b25afaa5eb03c239e251cd222b1448e211376a4
acycoco <pink10257@naver.com>
2024-08-18 03:37:53 +0900

    feat: MemePost entity생성
    - MemeMeta(이미지에 대한 메타 정보)를 원래 분리하려다가
    MemePost생성시 너무 많은 의존성이 생기고, oneToOne관계이기 때문에 꼭 분리할 필요가 없다고 생각이 들었음
    - 또한 MemePost 단건 조회 시 항상 메타데이터가 필요하므로 과도한 정규화라는 생각이 들어서 단일 테이블로 관리
    
    related-to: #21

---
+++ src/main/java/com/findmymeme/memepost/domain/MemePost.java
@@ -0,0 +1,55 @@
+package com.findmymeme.memepost.domain;
+
+import com.findmymeme.BaseEntity;
+import com.findmymeme.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemePost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String extension;
+
+    private Resolution resolution;
+
+    @Column(nullable = false)
+    private Long size;
+
+    @Column(nullable = false)
+    private String originalFilename;
+
+    @Column(nullable = false)
+    private Long likeCount;
+
+    @Column(nullable = false)
+    private Long viewCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public MemePost(String imageUrl, String extension, Resolution resolution, Long size, String originalFilename, User user) {
+        this.imageUrl = imageUrl;
+        this.extension = extension;
+        this.resolution = resolution;
+        this.size = size;
+        this.originalFilename = originalFilename;
+        this.likeCount = 0L;
+        this.viewCount = 0L;
+        this.user = user;
+    }
+}
