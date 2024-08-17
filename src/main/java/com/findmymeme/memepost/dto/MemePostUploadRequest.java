42893859c929950cbbbbdf2c2a35bfd53996646a
acycoco <pink10257@naver.com>
2024-08-18 03:49:21 +0900

    feat: MemePostUpload Request, Response dto생성
    related-to: #23

---
+++ src/main/java/com/findmymeme/memepost/dto/MemePostUploadRequest.java
@@ -0,0 +1,25 @@
+package com.findmymeme.memepost.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class MemePostUploadRequest {
+
+    @NotBlank
+    private String imageUrl;
+    @NotNull
+    private List<Long> tags;
+
+    @Builder
+    public MemePostUploadRequest(String imageUrl, List<Long> tags) {
+        this.imageUrl = imageUrl;
+        this.tags = tags;
+    }
+}
