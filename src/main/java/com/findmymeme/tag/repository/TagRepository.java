1cf20bfcf08d3e3a9e9bef80dcd6790b530b6df0
acycoco <pink10257@naver.com>
2024-08-17 20:10:04 +0900

    feat: TagRepository 생성
    related-to: #51

---
+++ src/main/java/com/findmymeme/tag/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.findmymeme.tag.repository;
+
+import com.findmymeme.tag.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
