c0b7f1cccc8bf5d4987d18e2be48f620ea363870
acycoco <pink10257@naver.com>
2024-08-18 03:45:10 +0900

    feat: fileupload 시 FileMeta 메타데이터추출, 저장
    - related-to: #56

--- src/main/java/com/findmymeme/file/service/FileService.java
+++ src/main/java/com/findmymeme/file/service/FileService.java
@@ -5,12 +5,17 @@ import com.findmymeme.exception.FindMyMemeException;
 import com.findmymeme.file.domain.FileMeta;
 import com.findmymeme.file.repository.FileMetaRepository;
 import com.findmymeme.file.dto.FileUploadResponse;
+import com.findmymeme.memepost.domain.Resolution;
 import com.findmymeme.user.User;
 import com.findmymeme.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 @Service
 @Transactional
@@ -40,9 +45,13 @@ public class FileService {
     }
 
     private FileMeta saveFileMeta(MultipartFile file, String fileUrl, User user) {
+        String originalFilename = file.getOriginalFilename();
         FileMeta fileMeta = FileMeta.builder()
-                .originalFilename(file.getOriginalFilename())
+                .originalFilename(originalFilename)
                 .fileUrl(fileUrl)
+                .size(file.getSize())
+                .resolution(getResolution(file))
+                .extension(fileStorageService.extractExtension(originalFilename))
                 .user(user)
                 .build();
         return fileMetaRepository.save(fileMeta);
@@ -52,5 +61,19 @@ public class FileService {
         if (tempUrl != null) {
             fileStorageService.deleteTempFile(tempUrl);
         }
+    }
+
+    private Resolution getResolution(MultipartFile file) {
+        try {
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            if (image != null) {
+                int width = image.getWidth();
+                int height = image.getHeight();
+                return new Resolution(width, height);
+            }
+        } catch (IOException e) {
+            throw new FindMyMemeException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+        return new Resolution(0, 0);
     }
 }
