536ab35a4558e2959a9df164935625a2722fd301
acycoco <pink10257@naver.com>
2024-08-18 18:36:37 +0900

    feat: 단건조회 MemePostGetResponse dto생성
    related-to: #60

---
+++ src/main/java/com/findmymeme/memepost/dto/MemePostGetResponse.java
@@ -0,0 +1,67 @@
+package com.findmymeme.memepost.dto;
+
+import com.findmymeme.memepost.domain.MemePost;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class MemePostGetResponse {
+
+    private Long id;
+    private String imageUrl;
+    private String extension;
+    private int height;
+    private int weight;
+    private Long size;
+    private String originalFilename;
+    private Long likeCount;
+    private Long viewCount;
+    private String username;
+    private boolean owner;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<String> tags;
+
+    @Builder
+    public MemePostGetResponse(Long id, String imageUrl, String extension,
+                               int height, int weight, Long size, String originalFilename,
+                               Long likeCount, Long viewCount, String username,
+                               LocalDateTime createdAt, LocalDateTime updatedAt, List<String> tags) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.extension = extension;
+        this.height = height;
+        this.weight = weight;
+        this.size = size;
+        this.originalFilename = originalFilename;
+        this.likeCount = likeCount;
+        this.viewCount = viewCount;
+        this.username = username;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.tags = tags;
+
+    }
+
+    public MemePostGetResponse(MemePost memePost, boolean isOwner, List<String> tags) {
+        this.id = memePost.getId();
+        this.imageUrl = memePost.getImageUrl();
+        this.extension = memePost.getExtension();
+        this.height = memePost.getResolution().getHeight();
+        this.weight = memePost.getResolution().getWidth();
+        this.size = memePost.getSize();
+        this.originalFilename = memePost.getOriginalFilename();
+        this.likeCount = memePost.getLikeCount();
+        this.viewCount = memePost.getViewCount();
+        this.username = memePost.getUser().getUsername();
+        this.owner = isOwner;
+        this.createdAt = memePost.getCreatedAt();
+        this.updatedAt = memePost.getUpdatedAt();
+        this.tags = tags;
+    }
+}
