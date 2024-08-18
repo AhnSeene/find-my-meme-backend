6b2a7b16b7bfe07f2fa6504df25d25e13610f8e0
acycoco <pink10257@naver.com>
2024-08-18 18:39:16 +0900

    feat: PostTagRepository tag fetch join메서드 생성
    - postId, postType으로 post의 tag조회시 tag도 항상 같이 필요하므로 fetch join
    
    related-to: #60

--- src/main/java/com/findmymeme/tag/repository/PostTagRepository.java
+++ src/main/java/com/findmymeme/tag/repository/PostTagRepository.java
@@ -1,7 +1,14 @@
 package com.findmymeme.tag.repository;
 
 import com.findmymeme.tag.domain.PostTag;
+import com.findmymeme.tag.domain.PostType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostTagRepository extends JpaRepository<PostTag, Long> {
+
+    @EntityGraph(attributePaths = {"tag"})
+    List<PostTag> findAllByPostIdAndPostType(Long postId, PostType postType);
 }
