41209fa64f9f427daf7e8cae61a77f8fbe335f96
acycoco <pink10257@naver.com>
2024-08-21 02:29:21 +0900

    feat: MemePostLikeReposotry 좋아요했는지 판단하는 메서드 생성
    related-to: #70

--- src/main/java/com/findmymeme/memepost/repository/MemePostLikeRepository.java
+++ src/main/java/com/findmymeme/memepost/repository/MemePostLikeRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemePostLikeRepository extends JpaRepository<MemePostLike, Long> {
+    Optional<MemePostLike> findByMemePostAndUser(MemePost memePost, User user);
     boolean existsByMemePostIdAndUserId(Long memePostId, Long userId);
 }
