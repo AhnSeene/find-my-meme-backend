dd15a479766807d4db95974315b0853a72f545aa
acycoco <pink10257@naver.com>
2024-08-21 02:12:27 +0900

    feat: MemePostLike entity 생성
    related-to: #70

---
+++ src/main/java/com/findmymeme/memepost/domain/MemePostLike.java
@@ -0,0 +1,35 @@
+package com.findmymeme.memepost.domain;
+
+import com.findmymeme.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        uniqueConstraints = @UniqueConstraint(columnNames = {"meme_post_id", "user_id"})
+)
+public class MemePostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meme_post_id", nullable = false)
+    private MemePost memePost;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public MemePostLike(MemePost memePost, User user) {
+        this.memePost = memePost;
+        this.user = user;
+    }
+}
