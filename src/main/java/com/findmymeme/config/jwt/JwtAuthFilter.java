fe732399e761fc254351c352fd970bb4d6773d0f
acycoco <pink10257@naver.com>
2024-08-21 02:09:03 +0900

    fix: JwtAuthFilter 두번 실행되는 문제 해결 @Component 제거
    related-to: #78

--- src/main/java/com/findmymeme/config/jwt/JwtAuthFilter.java
+++ src/main/java/com/findmymeme/config/jwt/JwtAuthFilter.java
@@ -13,7 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
