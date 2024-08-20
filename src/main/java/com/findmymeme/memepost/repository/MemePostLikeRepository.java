ced7fbaa66ea326f32ac9063aaeb6ae9d6128c81
acycoco <pink10257@naver.com>
2024-08-21 02:24:21 +0900

    feat: MemePostService 단건 조회 로그인한 경우 추가 isLiked 필드 추가
    - memePostLikeRepository에서 해당 유저가 좋아요를 했는지 조회
    
    related-to: #70

--- src/main/java/com/findmymeme/memepost/repository/MemePostLikeRepository.java
+++ src/main/java/com/findmymeme/memepost/repository/MemePostLikeRepository.java
@@ -1,4 +1,12 @@
 package com.findmymeme.memepost.repository;
+
+import com.findmymeme.memepost.domain.MemePost;
+import com.findmymeme.memepost.domain.MemePostLike;
+import com.findmymeme.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface MemePostLikeRepository extends JpaRepository<MemePostLike, Long> {
+    boolean existsByMemePostIdAndUserId(Long memePostId, Long userId);
 }
