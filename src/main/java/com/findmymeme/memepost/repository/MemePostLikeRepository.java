365e0a9ed62b35d09ac06b44cd282c05e553c4b6
acycoco <pink10257@naver.com>
2024-08-21 02:13:51 +0900

    feat: MemePostLikeRepository 생성
    related-to: #70

---
+++ src/main/java/com/findmymeme/memepost/repository/MemePostLikeRepository.java
@@ -0,0 +1,4 @@
+package com.findmymeme.memepost.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MemePostLikeRepository extends JpaRepository<MemePostLike, Long> {
+}
