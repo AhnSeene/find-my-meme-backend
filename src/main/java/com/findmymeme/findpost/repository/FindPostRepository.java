20487797ace480357f85091cbb25dbe119e32489
acycoco <pink10257@naver.com>
2024-08-22 03:58:41 +0900

    feat: FindPostRepository 단건 조회, 목록 조회시 삭제되지 않은 게시물만 조회하는 where절 추가
    related-to: #85# type: Subject(50 글자 이내)

--- src/main/java/com/findmymeme/findpost/repository/FindPostRepository.java
+++ src/main/java/com/findmymeme/findpost/repository/FindPostRepository.java
@@ -5,15 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface FindPostRepository extends JpaRepository<FindPost, Long> {
 
     @EntityGraph(attributePaths = {"user"})
-    Optional<FindPost> findWithUserById(Long id);
+    @Query("SELECT fp FROM FindPost fp WHERE fp.id = :id AND fp.deletedAt IS NULL")
+    Optional<FindPost> findWithUserById(@Param("id") Long id);
 
     @Override
     @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT fp FROM FindPost fp WHERE fp.deletedAt IS NULL")
     Page<FindPost> findAll(Pageable pageable);
 }
