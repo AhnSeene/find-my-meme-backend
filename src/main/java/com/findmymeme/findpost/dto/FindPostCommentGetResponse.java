2011b5f9054529f70a8e075f90e3df7d704cd6a4
acycoco <pink10257@naver.com>
2024-08-22 04:57:54 +0900

    feat: FindPostComment 조회 응답 dto에 deletedAt 추가, 삭제 시에 htmlContent내용 변경
    related-to: #85

--- src/main/java/com/findmymeme/findpost/dto/FindPostCommentGetResponse.java
+++ src/main/java/com/findmymeme/findpost/dto/FindPostCommentGetResponse.java
@@ -12,6 +12,8 @@ import java.util.Optional;
 @NoArgsConstructor
 public class FindPostCommentGetResponse {
 
+    private static final String DELETED_COMMENT = "삭제된 댓글입니다.";
+
     private Long id;
     private Long parentCommentId;
     private Long findPostId;
@@ -20,6 +22,7 @@ public class FindPostCommentGetResponse {
     private Boolean selected;
     private boolean owner;
     private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
 
     public FindPostCommentGetResponse(FindPostComment comment, boolean isOwner) {
         this.id = comment.getId();
@@ -27,16 +30,18 @@ public class FindPostCommentGetResponse {
                 .map(FindPostComment::getId)
                 .orElse(null);
         this.findPostId = comment.getFindPost().getId();
-        this.htmlContent = comment.getHtmlContent();
+        this.htmlContent = comment.isDeleted() ? DELETED_COMMENT : comment.getHtmlContent();
         this.username = comment.getUser().getUsername();
         this.selected = comment.isSelected();
         this.owner = isOwner;
         this.createdAt = comment.getCreatedAt();
+        this.deletedAt = comment.getDeletedAt();
     }
 
     @Builder
     public FindPostCommentGetResponse(Long id, Long parentCommentId, Long findPostId, String htmlContent,
-                                      String username, boolean selected, boolean owner, LocalDateTime createdAt) {
+                                      String username, boolean selected, boolean owner, LocalDateTime createdAt,
+                                      LocalDateTime deletedAt) {
         this.id = id;
         this.parentCommentId = parentCommentId;
         this.findPostId = findPostId;
@@ -45,5 +50,6 @@ public class FindPostCommentGetResponse {
         this.selected = selected;
         this.owner = owner;
         this.createdAt = createdAt;
+        this.deletedAt = deletedAt;
     }
 }
