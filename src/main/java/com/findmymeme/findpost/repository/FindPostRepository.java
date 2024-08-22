3218996e3e4ed303c080706601c4eae4070d0497
acycoco <pink10257@naver.com>
2024-08-23 05:05:36 +0900

    feat: FindPostRepository 채택 상태로 조회하는 메서드 추가
    related-to: #69

--- src/main/java/com/findmymeme/findpost/repository/FindPostRepository.java
+++ src/main/java/com/findmymeme/findpost/repository/FindPostRepository.java
@@ -1,6 +1,7 @@
 package com.findmymeme.findpost.repository;
 
 import com.findmymeme.findpost.domain.FindPost;
+import com.findmymeme.findpost.domain.FindStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -20,6 +21,9 @@ public interface FindPostRepository extends JpaRepository<FindPost, Long> {
     @EntityGraph(attributePaths = {"user"})
     @Query("SELECT fp FROM FindPost fp WHERE fp.deletedAt IS NULL")
     Page<FindPost> findAll(Pageable pageable);
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT fp FROM FindPost fp WHERE fp.deletedAt IS NULL AND fp.findStatus = :findStatus")
+    Page<FindPost> findAllByFindStatus(Pageable pageable, FindStatus findStatus);
 
     @Query("SELECT fp FROM FindPost fp WHERE fp.deletedAt IS NULL AND fp.user.id = :userId")
     Page<FindPost> findAllByUserId(Pageable pageable, @Param("userId") Long userId);
