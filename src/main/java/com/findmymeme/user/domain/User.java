9cb1fd0b9638fd9db8d392f7d35869cee4005e92
acycoco <pink10257@naver.com>
2024-08-22 23:13:05 +0900

    feat: User entity profileImageUrl필드 추가, 업데이트 메서드 추가
    related-to: #88

--- src/main/java/com/findmymeme/user/domain/User.java
+++ src/main/java/com/findmymeme/user/domain/User.java
@@ -26,15 +26,22 @@ public class User extends BaseEntity {
     @Column(unique = true, nullable = false, length = 100)
     private String email;
 
+    private String profileImageUrl;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
 
     @Builder
-    public User(String username, String password, String email, Role role) {
+    public User(String username, String password, String email, String profileImageUrl, Role role) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.profileImageUrl = profileImageUrl;
         this.role = role;
+    }
+
+    public void updateProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
     }
 }
