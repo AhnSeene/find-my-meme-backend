e5fa299499ac3e154644d2ae9b8c98622e52c184
acycoco <pink10257@naver.com>
2025-01-29 21:24:57 +0900

    feat: /login, /reissue 응답 dto 변경 - 로그인 시 토큰 관련은 헤더와 쿠키로 응답보내도록 변경 - /reissue 시 응답 body 제거
    related-to: #117

--- src/main/java/com/findmymeme/user/dto/LoginResponse.java
+++ src/main/java/com/findmymeme/user/dto/LoginResponse.java
@@ -1,6 +1,5 @@
 package com.findmymeme.user.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,15 +8,14 @@ import lombok.NoArgsConstructor;
 public class LoginResponse {
 
     private String username;
-    private String accessToken;
-    private String refreshToken;
     private String role;
 
-    @Builder
-    public LoginResponse(String username, String accessToken, String refreshToken, String role) {
+    public LoginResponse(String username, String role) {
         this.username = username;
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
         this.role = role;
+    }
+
+    public static LoginResponse fromLoginDto(LoginDto loginDto) {
+        return new LoginResponse(loginDto.getUsername(), loginDto.getRole());
     }
 }
