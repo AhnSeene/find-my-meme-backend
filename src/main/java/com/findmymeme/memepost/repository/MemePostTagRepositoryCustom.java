afc2650425ce7558650b175ce2c0639be0102c7c
acycoco <pink10257@naver.com>
2025-02-01 23:19:41 +0900

    feat: MemePost의 tagId목록을 반환하는 findTagIdsByPostId 메서드 추가

--- src/main/java/com/findmymeme/memepost/repository/MemePostTagRepositoryCustom.java
+++ src/main/java/com/findmymeme/memepost/repository/MemePostTagRepositoryCustom.java
@@ -3,7 +3,9 @@ package com.findmymeme.memepost.repository;
 import com.findmymeme.memepost.dto.MemePostTagProjection;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemePostTagRepositoryCustom {
-    List<MemePostTagProjection> findTagNamesByPostId(List<Long> postIds);
+    List<MemePostTagProjection> findTagNamesInPostIds(List<Long> postIds);
+    List<Long> findTagIdsByPostId(Long postId);
 }
--- src/main/java/com/findmymeme/memepost/repository/MemePostTagRepositoryCustomImpl.java
+++ src/main/java/com/findmymeme/memepost/repository/MemePostTagRepositoryCustomImpl.java
@@ -5,7 +5,9 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.findmymeme.tag.domain.QMemePostTag.memePostTag;
@@ -16,7 +18,11 @@ public class MemePostTagRepositoryCustomImpl implements MemePostTagRepositoryCus
 
     private final JPAQueryFactory queryFactory;
     @Override
-    public List<MemePostTagProjection> findTagNamesByPostId(List<Long> postIds) {
+    public List<MemePostTagProjection> findTagNamesInPostIds(List<Long> postIds) {
+        if (CollectionUtils.isEmpty(postIds)) {
+            return Collections.emptyList();
+        }
+
         return queryFactory
                 .select(Projections.constructor(
                         MemePostTagProjection.class,
@@ -27,4 +33,13 @@ public class MemePostTagRepositoryCustomImpl implements MemePostTagRepositoryCus
                 .where(memePostTag.memePost.id.in(postIds))
                 .fetch();
     }
+
+    public List<Long> findTagIdsByPostId(Long postId) {
+        return queryFactory
+                .select(memePostTag.tag.id)
+                .from(memePostTag)
+                .where(memePostTag.memePost.id.eq(postId))
+                .fetch();
+    }
+
 }
