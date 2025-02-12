ea2200db37a3c36dc57be99cfbe9fcc5c061955c
acycoco <pink10257@naver.com>
2025-02-13 03:58:00 +0900

    feat: S3FileStorageService spring-cloud-aws 3.x버전으로 마이그레이션 (S3Client 적용) - AmazonS3 대신 S3Client로 변경 - Spring Boot 자동 설정 방식에 맞춰 코드 개선

--- src/main/java/com/findmymeme/file/service/S3FileStorageService.java
+++ src/main/java/com/findmymeme/file/service/S3FileStorageService.java
@@ -1,7 +1,5 @@
 package com.findmymeme.file.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.*;
 import com.findmymeme.exception.ErrorCode;
 import com.findmymeme.file.exception.FileStorageException;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +9,12 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Service
@@ -20,19 +22,19 @@ import java.io.IOException;
 public class S3FileStorageService implements FileStorageService {
     private static final String URL_FORMAT = "%s/%s";
     private static final String URL_SLASH = "/";
-    private final AmazonS3 amazonS3;
 
+    private final S3Client s3Client;
     private final String tempDir;
     private final String permanentDir;
     private final String bucket;
 
     public S3FileStorageService(
-            AmazonS3 amazonS3,
+            S3Client s3Client,
             @Value("${file.upload.temp-dir}") String tempDir,
             @Value("${file.upload.image-dir}") String permanentDir,
-            @Value("${cloud.aws.s3.bucket}") String bucket
+            @Value("${aws.s3.bucket}") String bucket
     ) {
-        this.amazonS3 = amazonS3;
+        this.s3Client = s3Client;
         this.tempDir = tempDir;
         this.permanentDir = permanentDir;
         this.bucket = bucket;
@@ -63,17 +65,26 @@ public class S3FileStorageService implements FileStorageService {
         String orgKey = tempDir + URL_SLASH + getFilename(tempFileUrl);
         String copyKey = permanentDir + URL_SLASH + getFilename(tempFileUrl);
 
-        CopyObjectRequest copyObjectRequest = new CopyObjectRequest(this.bucket, orgKey, this.bucket, copyKey);
-        copyObjectRequest.setCannedAccessControlList(CannedAccessControlList.PublicRead);
-        amazonS3.copyObject(copyObjectRequest);
+        CopyObjectRequest copyObjectRequest = CopyObjectRequest.builder()
+                .sourceBucket(bucket)
+                .sourceKey(orgKey)
+                .destinationBucket(bucket)
+                .destinationKey(copyKey)
+                .build();
+
+        s3Client.copyObject(copyObjectRequest);
 
         return copyKey;
     }
 
     @Override
     public Resource downloadFile(String fileUrl) {
-        GetObjectRequest getObjectRequest = new GetObjectRequest(bucket, fileUrl);
-        return new InputStreamResource(amazonS3.getObject(getObjectRequest).getObjectContent());
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileUrl)
+                .build();
+        InputStream inputStream = s3Client.getObject(getObjectRequest);
+        return new InputStreamResource(inputStream);
     }
 
     @Override
@@ -89,22 +100,26 @@ public class S3FileStorageService implements FileStorageService {
     private String storeFile(MultipartFile file, String dir) {
         String key = generateKey(dir, generateStoredFilename(file.getOriginalFilename()));
         try {
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getSize());
-            metadata.setContentType(file.getContentType());
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .acl(ObjectCannedACL.PUBLIC_READ)
+                    .build();
 
-            PutObjectRequest putObjectRequest = new PutObjectRequest(this.bucket, key, file.getInputStream(), metadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead);
-            amazonS3.putObject(putObjectRequest);
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
             return key;
-        } catch (AmazonS3Exception | IOException e) {
+        } catch (IOException e) {
             throw new FileStorageException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
 
-    private void deleteFile(String tempUrl) {
-        DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(this.bucket, tempUrl);
-        amazonS3.deleteObject(deleteObjectRequest);
+    private void deleteFile(String fileUrl) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileUrl)
+                .build();
+        s3Client.deleteObject(deleteObjectRequest);
     }
 
     private String generateKey(String dir, String filename) {
