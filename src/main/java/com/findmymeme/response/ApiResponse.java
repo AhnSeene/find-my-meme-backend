3baa86df8f4322a872f1290462dcbaaa90cbb54d
acycoco <pink10257@naver.com>
2025-04-14 21:15:25 +0900

    feat: api응답에 에러 종류를 구분하는 code 추가

--- src/main/java/com/findmymeme/response/ApiResponse.java
+++ src/main/java/com/findmymeme/response/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.findmymeme.response;
 
+import com.findmymeme.exception.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +12,22 @@ public class ApiResponse<T> {
 
     private final boolean success;
     private final String message;
+    private final String code;
     private final T data;
 
     public static <T> ApiResponse<T> success(String message, T data) {
         return ApiResponse.<T>builder()
                 .success(true)
                 .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode, T data) {
+        return ApiResponse.<T>builder()
+                .success(false)
+                .message(errorCode.getMessage())
+                .code(errorCode.name())
                 .data(data)
                 .build();
     }
