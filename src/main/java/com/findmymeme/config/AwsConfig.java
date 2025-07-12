7afe7ada5f1108edfec44fa5af38a403ca782b51
acycoco <pink10257@naver.com>
2025-07-13 02:44:17 +0900

    refactor(aws): LocalStack 연동을 위한 코드 통합 및 리팩토링
    LocalStack 도입에 맞춰 기존의 Profile 기반 환경 분리 코드를 제거하고,
    단일 서비스 구현체로 모든 환경을 지원하도록 코드를 통합합니다.
    
    related-to: #243

--- src/main/java/com/findmymeme/config/AwsConfig.java
+++ src/main/java/com/findmymeme/config/AwsConfig.java
@@ -1,17 +1,25 @@
 package com.findmymeme.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.providers.AwsRegionProvider;
 import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.LambdaClientBuilder;
+
+import java.net.URI;
 
 @Configuration
 public class AwsConfig {
 
+    @Value("${spring.cloud.aws.endpoint:#{null}}")
+    private String endpoint;
+
     /**
      * Spring Cloud AWS가 application.yml의 설정으로 자동 생성해주는
      * CredentialsProvider와 RegionProvider를 주입받아서 LambdaClient를 수동으로 생성합니다.
+     * 로컬 환경일 경우, 주입받은 endpoint로 설정을 오버라이드합니다.
      *
      * @param awsCredentialsProvider 자동 설정된 AWS 자격 증명 공급자
      * @param awsRegionProvider 자동 설정된 AWS 리전 공급자
@@ -20,9 +28,15 @@ public class AwsConfig {
     @Bean
     public LambdaClient lambdaClient(AwsCredentialsProvider awsCredentialsProvider,
                                      AwsRegionProvider awsRegionProvider) {
-        return LambdaClient.builder()
+        LambdaClientBuilder builder = LambdaClient.builder()
                 .credentialsProvider(awsCredentialsProvider)
-                .region(awsRegionProvider.getRegion())
-                .build();
+                .region(awsRegionProvider.getRegion());
+
+        if (endpoint != null) {
+            builder.endpointOverride(URI.create(endpoint));
+        }
+
+        return builder.build();
+
     }
 }
